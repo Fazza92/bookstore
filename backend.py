109583016300c839d492c02a37fc04fe92f29377
Oscar <oscarwasil@gmail.com>
2021-02-07 16:13:49 -0600

    Added search,delete,update functions to backend

--- backend.py
+++ backend.py
@@ -22,5 +22,30 @@ def view():
   conn.close()
   return rows
 
+def search(title="",author="",year="",isbn=""):
+  conn=sqlite3.connect("books.db")
+  cur=conn.cursor()
+  cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title,author,year,isbn)))
+  conn.commit()
+  conn.close()
+  
+
+
+def delete(id):
+  conn=sqlite3.connect("books.db")
+  cur=conn.cursor()
+  cur.execute("DELETE FROM book WHERE id=?",(id,))
+  conn.commit()
+  conn.close()
+  
+
+def update(id,title,author,year,isbn):
+  conn=sqlite3.connect("books.db")
+  cur=conn.cursor()
+  cur.execute("UPDATE book SET title=?,author=?,year=?,isbn=? WHERE id=?",(title,author,isbn,id))
+  conn.commit()
+  conn.close()
 
 connect()
+insert()
+print(view())
